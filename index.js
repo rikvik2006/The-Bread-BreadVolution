1bd8965d7f49c01449df5bafffe987dbe4163c44
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-02-25 23:01:22 +0100

    Created the slash commands handler and the execute of text commnads

--- index.js
+++ index.js
@@ -69,3 +69,58 @@ const functionFile = fs.readdirSync("./functions").filter(file => file.endsWith(
 for (const file of functionFile) {
     require(`./functions/${file}`);
 }
+
+
+
+// Command execute
+
+client.on('messageCreate', message => {
+    if (!message.content.startsWith(process.env.PREFIX) || message.author.bot) return;
+
+    const args = message.content.slice(process.env.PREFIX.length).split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    if (!client.commands.has(commandName) && !client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName))) {
+        return message.reply({ content: "Command not found", ephemeral: true });
+    }
+    const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+    // if (command.onlyStaff) {
+    //     if (!message.member.roles.cache.find(r => r.id === "923527745085005824") && !message.member.roles.cache.find(r => r.id === "923534959468249160")) {
+    //         return message.channel.send(`You don't have the permission to use this command!`);
+    //     }
+    // }
+
+    if (command.guildOnly && message.channel.type !== 'text') {
+        return message.reply('I can\'t execute that command inside DMs!');
+    }
+
+    try {
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
+    }
+});
+
+
+//Slash Command Eandler
+
+client.on("ready", () => {
+    client.guilds.cache.forEach(guild => {
+        client.commands.forEach(command => {
+            guild.commands.create(command.data)
+        })
+    })
+})
+
+
+
+client.on("interactionCreate", interaction => {
+    if (!interaction.isCommand()) return
+
+    const command = client.commands.get(interaction.commandName)
+    if (!command) return
+
+    command.execute(interaction)
+})
