e1579ee426eef3cc10687dd8b7a3e15bb4d1789f
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-03-03 19:25:43 +0100

    Server Info Comans create

---
+++ commands/utility/serverInfo.js
@@ -0,0 +1,35 @@
+module.exports = {
+    name: "serverinfo",
+    data: {
+        name: "serverinfo",
+        description: "Get information about the server",
+    },
+    execute(interaction) {
+
+        var server = interaction.guild;
+
+        var botCount = server.members.cache.filter(member => member.user.bot).size;
+        var utentiCount = server.memberCount - botCount;
+
+        var categoryCount = server.channels.cache.filter(c => c.type == "category").size
+        var textCount = server.channels.cache.filter(c => c.type == "text").size
+        var voiceCount = server.channels.cache.filter(c => c.type == "voice").size
+
+        var server_info_embed = new Discord.MessageEmbed()
+            .setTitle(server.name)
+            .setThumbnail(server.iconURL())
+            .setDescription("All info on this server")
+            .setThumbnail(server.iconURL())
+            .addField("Owner", "```" + server.ownerId.tag + "```", true)
+            .addField("Server id", "```" + server.id + "```", true)
+            .addField("Server region", "```" + server.region + "```", true)
+            .addField("Members", "```Total: " + server.memberCount + " - Users: " + utentiCount + " - Bots: " + botCount + "```", false)
+            .addField("Channels", "```Category: " + categoryCount + " - Text: " + textCount + " - Voice: " + voiceCount + "```", false)
+            .addField("Server created", "```" + server.createdAt.toDateString() + "```", true)
+            .addField("Boost level", "```Level " + server.premiumTier + " (Boost: " + server.premiumSubscriptionCount + ")```", true)
+
+
+
+        interaction.reply({ embeds: [server_info_embed] })
+    }
+}
