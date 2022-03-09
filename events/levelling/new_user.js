d06edf14a5f8722d03e26e44c9c9f9e1fe76c45c
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-03-09 23:30:54 +0100

    finished new user events

--- events/levelling/new_user.js
+++ events/levelling/new_user.js
@@ -11,5 +11,42 @@ module.exports = {
             .setTitle("No dm")
             .setDescription("You can't use this command in a DM")
             .setThumbnail(message.client.user.avatarURL())
+
+        con.query("SELECT * FROM  userstats", (err, result) => {
+            var userstatsList = result;
+
+            var index = userstatsList.findIndex(x => x.id == message.author.id);
+            if (index < 0) {
+                index = userstatsList.length;
+
+                userstatsList[index] = {
+                    id: message.author.id,
+                    username: message.member.user.tag,
+                    xp: 0,
+                    level: 0,
+                    cooldownXp: 0
+                }
+                
+                con.query(`INSERT INTO userstats VALUES ('${message.author.id}', '${message.member.user.tag}', 0, 0, 0)`)
+            }
+
+            var userstats = userstatsList[index];
+
+            if (userstats.cooldownXp <= 0) {
+                userstatasl.coolddownXP = 60; 
+                var xp = Math.floor(Math.random() * (40 - 15 + 1)) + 15;
+                userstats.xp += xp;
+
+                if (userstats.xp >= calcoloXpNecessario(userstats.level + 1)) {
+                    userstats.level++;
+
+                    var channel = client.channels.chache.get("942724846201241631");
+                    channel.send(`${message.member.user.tag} you have reached teh level ${userstats.level}`)
+
+                }
+
+                con.query(`UPDATE userstats SET level = ${userstats.level}, xp = ${userstats.xp}, cooldownXp = ${userstats.cooldownXp} WHERE id = ${userstats.id}`)
+            }
+        })
     }
 }
