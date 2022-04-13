9b24830d0d99525c505a8222291179667bb58fc3
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-04-13 23:56:48 +0200

    Created new mongo scheme ad created (work in progress) comamnd setTimeZone

---
+++ commands/slashCommands/utils/setWelcome.js
@@ -0,0 +1,46 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const GuildSettings = require("../../../models/GuildSettings");
+const { Permissions } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("setwelcomechannel")
+        .setDescription("Set the welcome channel for the server")
+        .addChannelOption(option => option
+            .setName("channel")
+            .setDescription("The channel to set as the welcome channel")
+            .setRequired(true)
+        ),
+    async execute(interaction) {
+
+        if (!interaction.member.permission.has([Permissions.FLAGS.ADMINISTRATOR])) {
+            return interaction.reply("You do not have permission to use this command.");
+        }
+
+        GuildSettings.findOne({ guild_id: interaction.guild.id }, (err, settings) => {
+            if (err) {
+                console.log(err);
+                return interaction.reply("An error occurred while trying to set the welcome channel. This error has been reported to the developers");
+            }
+
+            if (!settings) {
+                settings = new GuildSettings({
+                    guild_id: interaction.guild.id,
+                    welcome_channel_id: interaction.options.getChannel("channel").id
+                })
+            } else {
+                settings.welcome_channel_id = interaction.options.getChannel("channel").id;
+            }
+
+            settings.save(err => {
+                if (err) {
+                    console.log(err);
+                    return interaction.reply("An error occurred while trying to set the welcome channel.");
+                }
+
+                interaction.reply(`Welcome channel set to <#${interaction.options.getChannel("channel")}>`);
+
+            })
+        })
+    }
+}
