7c237083828cd40e9619429796c90df0810f1301
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-05-14 23:54:33 +0200

    finishing the clear commands

---
+++ commands/slashCommands/utils/clear.js
@@ -0,0 +1,60 @@
+const Discord = require("discord.js");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("clear")
+        .setDescription("Clears a number of messages")
+        .addIntegerOption(option =>
+            option
+                .setName("ammount")
+                .setDescription("The number of messages to delete")
+                .setRequired(true)
+        ),
+    async execute(interaction) {
+
+        const no_permission = function (action, permission_needed) {
+            const no_permission_embed = new Discord.MessageEmbed()
+                .setColor("#F04848")
+                .setTitle("No permission")
+                .setDescription(`You don't have permission to ${action}`)
+                .addField("Permission", `\`\`\`${permission_needed}\`\`\``)
+
+            interaction.reply({ embeds: [no_permission_embed], ephemeral: true })
+        }
+
+        if (!interaction.member.permissions.has("MANAGE_MESSAGES")) {
+            return no_permission("delete messages", "MANAGE MESSAGES")
+        }
+
+        if (!interaction.guild.me.permissions.has("MANAGE_MESSAGES")) {
+            var no_me_permissions_embed = new Discord.MessageEmbed()
+                .setTitle("No permission")
+                .setDescription("I don't have the permission to delete messages")
+                .addField("Permission", `\`\`\`MANAGE MESSAGES\`\`\``)
+                .setColor("#F04848")
+            return interaction.reply({ embeds: [no_me_permissions_embed] })
+        }
+
+        var ammount = interaction.options.getInteger("ammount") || 0
+
+        if (ammount > 100) {
+            var too_much_embed = new Discord.MessageEmbed()
+                .setDescription("I can't delete more than 100 message per time")
+                .setColor("#F04848")
+
+            return interaction.reply({ embeds: [too_much_embed] })
+        }
+
+        const messages_deleted_embed = new Discord.MessageEmbed()
+            .setDescription(`Deleted ${ammount} messages`)
+            .setColor("#2D2D2D")
+
+        interaction.bulkDelete(ammount, true)
+        interaction.reply({ embeds: [messages_deleted_embed] }).then(msg => {
+            msg.delete({ timeout: 5000 })
+        })
+
+
+    }
+}
