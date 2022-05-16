6c8a4d30459dce8d5bf61ee9d0d418de47f8f21a
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-05-16 19:30:08 +0200

    Created the serverInfo comamnds, later i will add the guildSettigs and the guildConfig in there

---
+++ commands/slashCommands/utils/info/sreverInfo.js
@@ -0,0 +1,61 @@
+const Discord = require("discord.js");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const GuildConfig = require("../../../../models/GuildConfig");
+const GuildSettings = require("../../../../models/GuildSettings");
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("serverinfo")
+        .setDescription("Gets information about the server"),
+    
+    async execute (interaction) {
+        
+        const server = interaction.guild;
+
+        const owner_member = interaction.guild.members.cache.get(server.ownerId);
+
+        var botCount = server.members.cache.filter(member => member.user.bot).size;
+        var userCount = server.memberCount - botCount;
+
+        var categoryCount = server.channels.cache.filter(c => c.type == "category").size
+        var textCount = server.channels.cache.filter(c => c.type == "text").size
+        var voiceCount = server.channels.cache.filter(c => c.type == "voice").size
+        var roleCount = server.roles.cache.size;
+
+        if (server.premiumTier === "NONE") {
+            server.premiumTier = "No boost";
+        }
+
+
+        var server_info_embed = new Discord.MessageEmbed()
+            .setThumbnail(server.iconURL())
+            .setColor("#2D2D2D")
+            .addField("Server Name", server.name, true)
+            .addField("Server ID", server.id, true)
+            .addField("Guild Owner", `${owner_member.tag}`, true)
+            .addField("Boost", server.premiumTier || "No boost", true)
+            .addField("Channels", textCount + voiceCount + " Channels", true)
+            .addField("Roles", roleCount + " Roles", true)
+            .addField("Categories", categoryCount + " Categories", true)
+            .addField ("Members", userCount + " Members", true)
+
+
+
+
+
+
+
+            // .addField("Owner", "```" + server.ownerId.tag + "```", true)
+            // .addField("Server id", "```" + server.id + "```", true)
+            // .addField("Server region", "```" + server.region + "```", true)
+            // .addField("Members", "```Total: " + server.memberCount + " - Users: " + utentiCount + " - Bots: " + botCount + "```", false)
+            // .addField("Channels", "```Category: " + categoryCount + " - Text: " + textCount + " - Voice: " + voiceCount + "```", false)
+            // .addField("Server created", "```" + server.createdAt.toDateString() + "```", true)
+            // .addField("Boost level", "```Level " + server.premiumTier + " (Boost: " + server.premiumSubscriptionCount + ")```", true)
+
+
+
+        interaction.reply({ embeds: [server_info_embed], ephemeral: true })
+    }
+}
