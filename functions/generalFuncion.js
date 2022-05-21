b3e98849314bedd8a0a835957edfcbe5c2c2e670
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-05-21 18:13:32 +0200

    added a new color palete to the bot

--- functions/generalFuncion.js
+++ functions/generalFuncion.js
@@ -4,6 +4,39 @@ global.sendTestEmoji = function (message) {
 
 function sendTestEmoji2(message) {
     message.reply("🍞")
-}
+};
+
+global.yellow_bred = "#F9A62B";
+
+global.grey_bread = "#2D2D2D"
+
+global.red_bred = "#F04848"
+
+global.NotSoBluple = "#5866Ef"
+
+global.OnlineGreen = "#3DA560"
+
+global.IdleYellow = "#F9A62B"
+
+global.DndRed = "#ec4145"
+
+global.BraveryPurple = "#9b84ec"
+
+global.BrillanceColar = "#f37b68"
+
+global.BalanceTurqoise = "#49ddc1"
+
+global.NitroGrey = "#5f5d7e"
+
+global.BoostPink = "#f373f6"
+
+global.StreammerPurple = "#583694"
+
+global.HyperlinkBlue = "#09b9f2"
+
+global.DarkGrey = "#2f3136"
+
+global.NotQuiteBlack = "#23272a"
+
 
 
