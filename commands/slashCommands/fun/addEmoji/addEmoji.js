850ee60a89081ac3dbecb48ec3ead4fba8e11fd1
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-06-30 13:44:47 +0200

    Created addemoji copy commands

--- commands/slashCommands/fun/addEmoji/addEmoji.js
+++ commands/slashCommands/fun/addEmoji/addEmoji.js
@@ -20,7 +20,46 @@ module.exports = {
                     option
                         .setName("name")
                         .setDescription("Name of emoji")
+                        .setRequired(true)
                 )
         ),
 
+    async execute(interaction) {
+        if (interaction.options.getSubcommand() === "copy") {
+            let emoji = interaction.options.getString("emoji").trim()
+            console.log(emoji)
+            let name = interaction.options.getString("name")
+
+
+
+            if (emoji.startsWith("<") && emoji.endsWith(">")) {
+                const id = emoji.match(/\d{15,}/g)[0]
+
+                const type = await axios.get(`https://cdn.discordapp.com/emojis/${id}.gif`)
+                    .then(image => {
+                        if (image) return "gif"
+                        else return "png"
+                    }).catch(err => {
+                        return "png"
+                    })
+
+                emoji = `https://cdn.discordapp.com/emojis/${id}.${type}?quality=lossless`
+            }
+
+            interaction.guild.emojis.create(emoji, name)
+                .then(emoji => {
+                    const added_emoji = new Discord.MessageEmbed()
+                        .setAuthor({ name: `${interaction.user.tag} added an emoji`, iconURL: interaction.user.displayAvatarURL() })
+                        .setDescription(`**Emoji:** ${emoji.toString()}\n**Name:** \`${emoji.name}\``)
+                        .setColor("#2d2d2d")
+
+                    return interaction.reply({ embeds: [added_emoji] })
+                }).catch(err => {
+                    const err_emoji = new Discord.MessageEmbed()
+                        .setDescription("There are an erore while adding you emoji")
+                        .setColor(red_bread)
+                    return interaction.reply(err_emoji)
+                })
+        }
+    }
 }
