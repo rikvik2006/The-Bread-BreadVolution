93b11e7468b04e8483a8ff43620e5fe14faa3ed5
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-07-03 15:51:28 +0200

    Setted up top.gg auto poster

--- events/events1/ready/topggApiPost.js
+++ events/events1/ready/topggApiPost.js
@@ -1,10 +1,22 @@
-const Topgg = require("@top-gg/sdk")
-const api = new Topgg.Api(process.env.TOPGG_TOKEN)
+const Topgg = require("@top-gg/sdk");
+const { createSpinner } = require("nanospinner");
+const topgg_token = process.env.TOPGG_TOKEN || null;
+const { AutoPoster } = require("topgg-autoposter")
 
 module.exports = {
     name: "ready",
 
-    async execute() {
-        console.log("Posted");
+    async execute(client) {
+
+        const spinner = createSpinner("Posting stats to Top.gg").start()
+        if (topgg_token === null) {
+            return spinner.error({ text: "Nothing posted to Top.gg" })
+        } else {
+            const ap = AutoPoster.apply(topgg_token, client)
+
+            ap.on("posted", () => {
+                spinner.success({ text: "Posted stats to Top.gg" })
+            })
+        }
     }
 }
