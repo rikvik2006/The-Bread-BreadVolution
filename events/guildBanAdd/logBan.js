a423b8840396a882c53e973da23b9c91412252c3
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-07-26 01:20:42 +0200

    Created frist moderation log (ban log)

---
+++ events/guildBanAdd/logBan.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const GuildConfig = require("../../models/GuildConfig");
+
+module.exports = {
+    name: "guildBanAdd",
+    async execute(ban) {
+
+        let data;
+
+        try {
+            data = await GuildConfig.findOne({ guildId: ban.guild.id })
+
+            if (!data) {
+                data = await GuildConfig.create({ guildId: guild.ban.id })
+            }
+        } catch (err) {
+            console.log(err);
+        }
+
+        if (data.modLogChannel === undefined) return
+
+        const channel = client.channels.cache.get(data.modLogChannel);
+
+        const ban_log = new Discord.MessageEmbed()
+            .setColor(red_bread)
+            .setAuthor({ name: ban.user.tag, iconURL: ban.user.displayAvatarURL() })
+            .setDescription(`**🔒<@${ban.user.id}> was banned**`)
+
+        channel.send({ embeds: [ban_log] })
+
+    }
+}
